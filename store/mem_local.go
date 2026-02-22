bea2a672162dc9fd887a7767c3fa0a997be5d031
vinayvhk <vinayhegde@outlook.com>
2026-02-22 08:30:07 +0000

    added channel code

--- store/mem_local.go
+++ store/mem_local.go
@@ -1,6 +1,7 @@
 package store
 
 type Store interface {
+	Init() error
 	Set(string, string) bool
 	Get(string) (string, bool)
 }
@@ -8,6 +9,12 @@ type Store interface {
 type MemLocalStore struct {
 	Data map[string]string 
 }
+
+func (s *MemLocalStore) Init() error {
+	s.Data = make(map[string]string)
+	return nil
+}
+
 
 func (s *MemLocalStore) Set(k string, v string) bool {
 	if s.Data == nil {
