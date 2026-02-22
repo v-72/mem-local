bad92c8d5857ba00dbf64ed5055c6a7e9abe52e1
vinayvhk <vinayhegde@outlook.com>
2026-02-22 13:35:53 +0000

    added example

--- example.go
+++ example.go
@@ -1,17 +1,40 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 
 	"github.com/v-72/mem-local/store"
 )
 
+func generateSecureString(nBytes int) (string, error) {
+	b := make([]byte, nBytes)
+	_, err := rand.Read(b)
+	if err != nil {
+		return "", err
+	}
+	return base64.URLEncoding.EncodeToString(b), nil
+}
+
 func main() {
 	fmt.Println("init")
 	s := &store.MemLocalStore{}
-	setStatus := s.Set("test", "test")
-	fmt.Println("setStatus:", setStatus)
-	val, _ := s.Get("test")
+	err := s.Init()
+
+	if err != nil {
+		fmt.Println("Error initializing local store:", err)
+		return
+	}
+
+	key, _ := generateSecureString(8)
+	value, _ := generateSecureString(64)
+	fmt.Printf("Generated key: %s\n", key)
+	fmt.Printf("Generated value: %s\n", value)
+
+	setStatus := s.Set(key, value)
+	fmt.Println("\nsetStatus:", setStatus)
+	val, _ := s.Get(key)
 	fmt.Println(val)
 	fmt.Println("value from cache", val)
 }
